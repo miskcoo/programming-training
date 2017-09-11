e6456a8ff83e44db0510f2053396f0b361243ad4
Yuhao Zhou <miskcoo@gmail.com>
2017-09-11 16:20:58 +0800

    Add doc.

--- wiki-index/server/server/models.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf8 -*-
-
-from django.db import models
-
-class Infobox(models.Model):
-    query_str = models.CharField()
-    json_str = models.CharField()
-    title = models.CharField()
